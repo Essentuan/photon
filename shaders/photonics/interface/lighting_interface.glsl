9e3c340e71c6e56392a758c5db734a0b8bea2491
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-07-15 08:14:29 -0500

    Use correct sun/sky values

--- shaders/photonics/interface/lighting_interface.glsl
+++ shaders/photonics/interface/lighting_interface.glsl
@@ -4,31 +4,28 @@ uniform float worldTime;
 uniform sampler2D colortex4;
 
 const float blocklight_scale = 6.0f;
-const float rcp_blocklight_scale = 1.0f /blocklight_scale;
+const float rcp_blocklight_scale = 1.0f / blocklight_scale;
 
 vec3 get_sun_direction() {
     return light_dir;
 }
 
-vec3 get_light_color() {
-#if defined OVERWORLD
-#ifdef SH_SKYLIGHT
-    return texelFetch(colortex4, ivec2(191, 11), 0).rgb * rcp_blocklight_scale;
-#else
-    return texelFetch(colortex4, ivec2(191, 1), 0).rgb * rcp_blocklight_scale;
-#endif
-#else
-    return mix(texelFetch(colortex4, ivec2(191, 1), 0).rgb, vec3(1.0f), 0.5) * rcp_blocklight_scale;
-#endif
-}
-
 vec3 get_sky_color(vec3 player_pos, vec3 direction) {
-    const float sun_inverse = 1.0f / SUN_I;
-    return get_light_color() * sun_inverse * SKYLIGHT_I * 0.5f;
+#if defined WORLD_OVERWORLD
+#ifdef SH_SKYLIGHT
+    vec3 skylight = texelFetch(colortex4, ivec2(191, 11), 0).rgb;
+#else
+    vec3 skylight = texelFetch(colortex4, ivec2(191, 1), 0).rgb;
+#endif
+#else
+    vec3 skylight = mix(texelFetch(colortex4, ivec2(191, 1), 0).rgb, vec3(1.0f), 0.5);
+#endif
+
+    return skylight * SKYLIGHT_I * rcp_blocklight_scale;
 }
 
-vec3 get_sun_color(vec3 player_pos, vec3 directioin) {
-    return get_light_color() * BOUNCED_LIGHT_I * 4.0f;
+vec3 get_sun_color(vec3 player_pos, vec3 direction) {
+    return texelFetch(colortex4, ivec2(191, 0), 0).rgb * BOUNCED_LIGHT_I * rcp_blocklight_scale;
 }
 
 #if defined OVERWORLD
